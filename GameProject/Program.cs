ef7cdb716c47063ecc887f19408e3200784a6033
Fatih Demir <fatih.demir@nisantasi.edu.tr>
2021-01-28 01:05:57 +0300

    280121_son

--- GameProject/Program.cs
+++ GameProject/Program.cs
@@ -9,7 +9,7 @@ namespace GameProject
             GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
             gamerManager.Add(new Gamer {Id=1,BirthYear=1984,FirstName="Fatih",LastName="Demir",IdentityNumber=12345 });
 
-            //Simule edildi
+            //Simule edildi 
         }
     }
 }
