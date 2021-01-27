91b88c8fa397436362e877a9b38f2054e3c22b56
Fatih Demir <fatihdmr.1057@gmail.com>
2021-01-28 00:44:12 +0300

    280121_son

--- GameProject/Program.cs
+++ GameProject/Program.cs
@@ -8,6 +8,8 @@ namespace GameProject
         {
             GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
             gamerManager.Add(new Gamer {Id=1,BirthYear=1984,FirstName="Fatih",LastName="Demir",IdentityNumber=12345 });
+
+            //Simule edildi
         }
     }
 }
